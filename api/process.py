e6b2d5be0acb451fdcac3d42bcf7c149eed2275c
Muhammad Wafa <wafax.4@gmail.com>
2021-04-25 17:16:44 +0700

    using example

--- api/process.py
+++ api/process.py
@@ -5,6 +5,8 @@ class handler(BaseHTTPRequestHandler):
 
   def do_GET(self):
     self.send_response(200)
+    self.send_header('Content-type','text/plain')
+    self.end_headers()
     self.wfile.write(
       str("it's work!").encode()
     )
