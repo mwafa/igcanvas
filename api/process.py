a601aec9326832009db403263b0e60a957a9042f
Muhammad Wafa <wafax.4@gmail.com>
2021-04-25 17:48:00 +0700

    process image to igcanvas

--- api/process.py
+++ api/process.py
@@ -1,5 +1,6 @@
 from http.server import BaseHTTPRequestHandler
-
+import cgi, igcanvas, time
+import os
 
 class handler(BaseHTTPRequestHandler):
 
@@ -11,3 +12,32 @@ class handler(BaseHTTPRequestHandler):
       str("it's work!").encode()
     )
     return
+  
+  def do_POST(self):
+    form = cgi.FieldStorage(
+        fp=self.rfile,
+        headers=self.headers,
+        environ={'REQUEST_METHOD':'POST',
+                  'CONTENT_TYPE':self.headers['Content-Type'],
+                  })
+    filename = form['file'].filename
+    data = form['file'].file.read()
+    filepath = "%s-%s" % (time.time(), filename)
+    with open(filepath, 'wb') as f:
+      f.write(data)
+    igcanvas.genImage(filepath).save(filepath)
+    self.send_response(200)
+    self.send_header('Content-type', 'image/jpeg')
+    self.end_headers()
+    with open(filepath, 'rb') as content:
+      self.wfile.write(content.read())
+    os.remove(filepath)
+    return
+  
+  def respond(self, s: str):
+    self.send_response(200)
+    self.send_header('Content-type','text/plain')
+    self.end_headers()
+    self.wfile.write(
+      str(s).encode()
+    )
---
+++ app.py
@@ -0,0 +1,16 @@
+from http.server import HTTPServer
+from api import process
+
+if __name__ == "__main__":
+    port = 3000 
+    hostname = 'localhost'
+    webServer = HTTPServer((hostname, port), process.handler)
+    print("Server started http://%s:%s" % (hostname, port))
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")
